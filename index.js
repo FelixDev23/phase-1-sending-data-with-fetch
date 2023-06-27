66bae22e947b2f4a81430b0f4e28c30557486a9d
**** <****>
2023-06-27 10:48:52 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+// //Add Event Listener
+// document.querySelector('h1').addEventListener('submit',handleSubmit) 
+
+
+function submitData(){
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers : {
+            'Content-Type' : "application/json",
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(
+            { name: 'Steve', 
+            email: 'steve@steve.com'
+        })
+    })
+    .then(res => { return res.json()})
+    .then(data => { console.log(data)})
+}
+
